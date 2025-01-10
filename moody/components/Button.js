758b9f77330e4e548c82ae9d2e4b6af2ea12529f
federico-ramirez <00009220@uca.edu.sv>
2025-01-09 20:56:22 -0600

    feat: create Login initial UI

--- moody/components/Button.js
+++ moody/components/Button.js
@@ -1,17 +1,18 @@
-import { Modak } from 'next/font/google';
+import { Fredoka } from 'next/font/google';
 import React from 'react'
 
-const modak = Modak({
+const fredoka = Fredoka({
     subsets: ["latin"],
-    weight: ["400"]
+    weight: ["700"]
 });
 
 export default function Button(props) {
-    const { text, dark } = props
+    const { text, dark, full } = props
   return (
     <button className={`rounded-full overflow-hidden duration-200 hover:opacity-60 border-2
-    border-solid border-indigo-600 ${dark ? 'text-white bg-indigo-600' : 'text-indigo-600'}`}>
-        <p className={`px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 ${modak.className}`}>{text}</p>
+    border-solid border-indigo-600 ${dark ? 'text-white bg-indigo-700 ' : 'text-indigo-600 '}
+    ${full ? 'grid place-items-center w-full' : ' '}`}>
+        <p className={`px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 ${fredoka.className}`}>{text}</p>
     </button>
   )
 }
--- moody/components/Login.js
+++ moody/components/Login.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import { Fredoka, Modak } from 'next/font/google';
+import React from 'react';
+import Button from './Button';
+
+const fredoka = Fredoka({
+    subsets: ["latin"],
+    weight: ["400"]
+});
+
+const modak = Modak({
+    subsets: ['latin'],
+    weight: ['400']
+});
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div className={`flex flex-col flex-1 justify-center items-center gap-4 ${fredoka.className}`}>
+            <h3 className={`text-4xl md:text-6xl sm:text-5xl ${modak.className}`}>Log In / Register</h3>
+            <p className='text-lg text-slate-600'>You're one step away to join <span className={`${modak.className} textGradient`}>Moody</span>!</p>
+            <input type='email' placeholder='Email' className='w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border-2 border-solid
+             border-indigo-400 rounded-full text-slate-600 outline-none duration-200 hover:border-indigo-600' required />
+            <input type='password' placeholder='Password' className='w-full max-w-[400px] mx-auto px-4 py-2 sm:py-3 border-2 border-solid 
+            border-indigo-400 rounded-full text-slate-600 outline-none duration-200 hover:border-indigo-600' required />
+            <div className='w-full max-w-[400px] mx-auto'>
+                <Button text='Submit' full />
+            </div>
+            <p className='text-center'>Don't have an account? <span className='text-indigo-600'>Sign up</span></p>
+        </div>
+    )
 }
